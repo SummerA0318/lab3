49bdd66c44dbec2e187a5b142400b0332e9dc7c8
SummerA0318 <yul201@ucsd.edu>
2022-10-12 14:51:12 -0700

    change method 1

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,8 +4,9 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+	  int[] copyarr = arr;
+	  for(int i = 0; i < arr.length; i += 1) {
+      arr[i] = copyarr[arr.length - i - 1];
     }
   }
 
